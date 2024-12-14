7261cdb7b9d80cc9ddfb54e1464d2329d0ae5b55
tejash023 <tejash34961@gmail.com>
2024-12-14 17:06:32 +0530

    optimized data provider class

--- src/test/java/utilities/DataProviders.java
+++ src/test/java/utilities/DataProviders.java
@@ -6,17 +6,18 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviders {
 
+    public static final String PATH = ".\\testData\\Opencart_LoginData.xlsx"; // Path to the Excel file
+    public static final String SHEET_NAME = "Sheet1"; // Name of the sheet
+
     //DataProvider 1
 
     @DataProvider(name="LoginData")
-    public String [][] getData() throws IOException
-    {
-        String path=".\\testData\\Opencart_LoginData.xlsx";//taking xl file from testData
+    public String [][] getData() throws IOException {
 
-        ExcelUtility excelUtility = new ExcelUtility(path);//creating an object for XLUtility
+        ExcelUtility excelUtility = new ExcelUtility(PATH);//creating an object for XLUtility
 
-        int totalRows = excelUtility.getRowCount("Sheet1");
-        int totalCols = excelUtility.getCellCount("Sheet1",1);
+        int totalRows = excelUtility.getRowCount(SHEET_NAME);
+        int totalCols = excelUtility.getCellCount(SHEET_NAME,1);
 
         String[][] loginData = new String[totalRows][totalCols];//created for two dimension array which can store the data user and password
 
@@ -24,7 +25,7 @@ public class DataProviders {
         {
             for(int j=0;j<totalCols;j++)  //0    i is rows j is col
             {
-                loginData[i-1][j]= excelUtility.getCellData("Sheet1",i, j);  //1,0
+                loginData[i-1][j]= excelUtility.getCellData(SHEET_NAME,i, j);  //1,0
             }
         }
         return loginData;//returning two dimension array
