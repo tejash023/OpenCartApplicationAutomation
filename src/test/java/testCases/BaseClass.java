a67c3032001a5f512817b59f515eab15a477de54
tejash023 <tejash34961@gmail.com>
2024-12-14 16:44:02 +0530

    setup tests on selenium grid

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -3,11 +3,14 @@ package testCases;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.apache.logging.log4j.Logger;
@@ -18,6 +21,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -25,7 +29,7 @@ import java.util.Properties;
 
 public class BaseClass {
 
-    public static WebDriver driver;
+    public WebDriver driver;
     public Logger logger;
     public Properties properties;
 
@@ -37,23 +41,59 @@ public class BaseClass {
         FileReader fileReader = new FileReader("./src//test//resources//config.properties");
         properties = new Properties();
         properties.load(fileReader);;
-
         logger = LogManager.getLogger(this.getClass());
-        switch (browser.toLowerCase()){
-            case "chrome" :
-                driver = new ChromeDriver();
-                break;
-            case "edge" :
-                driver = new EdgeDriver();
-                break;
-            case "firefox" :
-                driver = new FirefoxDriver();
-                break;
-            default:
-                driver = new ChromeDriver();
-                logger.info("Unsupported browser parameter, hence running tests on chrome");
-                break;
+
+        //Remote Execution
+        if(properties.getProperty("executionEnvironment").equalsIgnoreCase("remote")){
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+            //Setting up platform (WIN11, MAC)
+            if(operatingSystem.equalsIgnoreCase("windows")){
+                desiredCapabilities.setPlatform(Platform.WIN11);
+            }else if(operatingSystem.equalsIgnoreCase("mac")){
+                desiredCapabilities.setPlatform(Platform.MAC);
+            }else{
+                logger.error("unsupported platform");
+                return;
+            }
+
+            //Setting up browsers
+            switch (browser.toLowerCase()){
+                case "chrome" :
+                    desiredCapabilities.setBrowserName("chrome");
+                    break;
+                case "edge" :
+                    desiredCapabilities.setBrowserName("MicrosoftEdge");
+                    break;
+                case "firefox" :
+                    desiredCapabilities.setBrowserName("firefox");
+                    break;
+                default:
+                    logger.error("unsupported browser type");
+                    return;
+            }
+
+            driver = new RemoteWebDriver(new URL(properties.getProperty("hubURL")), desiredCapabilities);
         }
+        //Local Execution
+        if(properties.getProperty("executionEnvironment").equalsIgnoreCase("local")){
+            switch (browser.toLowerCase()){
+                case "chrome" :
+                    driver = new ChromeDriver();
+                    break;
+                case "edge" :
+                    driver = new EdgeDriver();
+                    break;
+                case "firefox" :
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    logger.info("Unsupported browser parameter, hence running tests on chrome");
+                    break;
+            }
+        }
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(properties.getProperty("appURL"));
